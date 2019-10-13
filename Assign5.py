e1febebd3e09a8216db06f45d78ab487c83bd368
Shaliniraj <56467118+Shaliniraj@users.noreply.github.com>
2019-10-13 14:02:16 +0530

    Assignment No 5

---
+++ Assign5.py
@@ -0,0 +1,11 @@
+num1 = int(input("Please enter a number"))
+
+if num1 > 99 and num1 < 1000:
+    print(num1, "The Number you have entered is a 3 digit number")
+elif num1 <=9:
+    print("The number you have entered is :",num1)        
+else:
+    print(num1, "The Number you have entered is a 2 digit number")
+
+        
+
