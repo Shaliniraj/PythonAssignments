f788e2acd4e306d8e31191973ba465ff08458e38
Shaliniraj <56467118+Shaliniraj@users.noreply.github.com>
2019-10-25 20:37:07 +0530

    Delete Assign9.py

--- Assign9.py
+++
@@ -1,5 +0,0 @@
-#This progranm prints the multiplication table of a number entered by the user using a for loop
-
-num = int(input("Please enter a number"))
-for i in range(1,11):
-    print(num,"x",i,"=",num*i)
