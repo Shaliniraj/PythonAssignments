e5fe48fd8636e5328508c52b0e81d9051f1c5b6f
Shaliniraj <56467118+Shaliniraj@users.noreply.github.com>
2019-11-01 14:53:16 +0530

    To_Print_sqaure

---
+++ Print_the_sqaure.py
@@ -0,0 +1,14 @@
+while True:
+
+    num1 = int(input("Please enter a 3 digit number"))
+
+    if num1 > 99 and num1 < 1000:
+        print(num1, "The Number you have entered is a 3 digit number")
+        print(num1 * num1)
+        
+    elif num1 <=9:
+        print("The number you have entered is :",num1)
+
+
+        
+
