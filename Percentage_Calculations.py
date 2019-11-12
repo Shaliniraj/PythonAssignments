a6d24072903f9e019ed0a23b793f660cd1db8d9f
Shaliniraj <56467118+Shaliniraj@users.noreply.github.com>
2019-11-12 22:13:02 +0530

    This program calculates the percentage of a student

--- Percentage_Calculations.py
+++ Percentage_Calculations.py
@@ -1,13 +1,19 @@
 """ This program shows the percentages of students scored in thier exams
 """
 
-def grades(percentage):
+def grades(percentage,scoreinenglish,scoreinmathematics,totalscoreinscience):
     if percentage >= 90:
         print("First class")
     elif percentage >= 75:
         print("Second class")
-    elif percentage <= 35:
+    elif scoreinenglish < 25:
         print("Fail")
+    elif scoreinmathematics < 35:
+        print("Fail")
+    elif totalscoreinscience < 35:
+        print("Fail")
+    elif percentage <= 35:
+        print("fail")
     else:
         print("Average")
 
@@ -15,20 +21,19 @@ def grades(percentage):
 while True:
     
     ScoreinEnglish = int(input("Please enter the marks scored in english "))
-    ScoreinScienceTheory = int(input("Please enter the marks scored in science Theoery  "))
-    ScoreinSciencePractical = int(input("Please enter the marks scored in science Practical  "))
-    TotalScoreScience = int(ScoreinScienceTheory + ScoreinSciencePractical)
-    print(TotalScoreScience)
-    ScoreinMathematics = int(input("Please enter the marks scored in Mathematics "))
 
-    if TotalScoreScience <= 33:
-        print("fail")
-    else:
-        Totalmarks =  int(ScoreinEnglish +  TotalScoreScience + ScoreinMathematics)
-        average = Totalmarks / 2.75
-        percentage = int((Totalmarks / 275) * 95)
-        print(percentage)
-        Marks = grades(percentage)
+    ScoreinMathematics = int(input("Please enter the marks scored in Mathematics "))
+        
+    ScoreinScienceTheory = int(input("Please enter the marks scored in science Theoery  "))
+
+    ScoreinSciencePractical = int(input("Please enter the marks scored in science Practical  "))
+
+    TotalScoreScience = int(ScoreinScienceTheory + ScoreinSciencePractical)
+
+    
+    Totalmarks =  int(ScoreinEnglish +  TotalScoreScience + ScoreinMathematics)
+    percentage = int((Totalmarks / 275) * 95)
+    Marks = grades(percentage,ScoreinEnglish,ScoreinMathematics,TotalScoreScience)
 
     
     
