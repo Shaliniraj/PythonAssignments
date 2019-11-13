f14dd64211ac931b3357f9e504b4ff4c8780edea
Shaliniraj <56467118+Shaliniraj@users.noreply.github.com>
2019-11-13 20:31:38 +0530

    first 12 even Fibonacci numbers

--- Fibonacci numbers..py
+++ Fibonacci numbers..py
@@ -1,24 +1,30 @@
-# Python program to print the first 12 "even" Fibonacci numbers.
-    
-numbers = int(input("How many numbers? "))
+#Program to display the first 12 even Fibonacci numbers
 
+nterms = 50
+
+# first two terms
 n1 = 1
 n2 = 1
 count = 0
+evennumber = 0
 
-if numbers <= 0:
+if nterms <= 0:
    print("Please enter a positive integer")
-elif numbers == 1:
-   print("Fibonacci numbers upto",numbers,":")
-   print(n1)
+   
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   
 else:
-   print("Fibonacci numbers upto",numbers,":")
-   while count < numbers:
-       print(n1,end=' , ')
-       n3 = n1 + n2
+   print("Fibonacci sequence:")
 
-       n1 = n2
-       n2 = n3
+   while count < nterms:
 
-       count += 1
-
+      if n1 % 2 == 0:
+         if evennumber != 12:
+            print(n1)
+            evennumber = evennumber+1
+      nth = n1 + n2
+      
+      n1 = n2
+      n2 = nth
+      count += 1
